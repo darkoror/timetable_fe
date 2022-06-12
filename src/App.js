7b03865d07df375751837b0b4e319ebda97d938d
dmytrozaremba <dmytrozaremba@gmail.com>
2022-06-12 19:58:57 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  axios.get('https://jsonplaceholder.typicode.com/posts').then((res) => {
+    console.log(res);
+  });
+  return <div className="App">Test</div>;
 }
 
 export default App;
