7ad050d7013ca2260d242e543f06075057f81ffb
dmytrozaremba <dmytrozaremba@gmail.com>
2022-07-11 19:17:46 +0300

    Remove more unused variables

--- src/components/Group.js
+++ src/components/Group.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Group = ({ item }) => {
-  const location = useLocation();
   return (
     <div>
       <Link to={`/groups/${item.id}/schedule`}>{item.name}</Link>
--- src/components/University.js
+++ src/components/University.js
@@ -1,14 +1,11 @@
 import React from 'react';
 
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const University = ({ item }) => {
-  const location = useLocation();
   return (
     <div>
-      <Link to={`${location.pathname}/${item.id}/departments`}>
-        {item.name}
-      </Link>
+      <Link to={`/universities/${item.id}/departments`}>{item.name}</Link>
     </div>
   );
 };
