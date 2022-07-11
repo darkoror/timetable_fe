2c5db531e498fa04791d52cc77d86bb2d977f143
dmytrozaremba <dmytrozaremba@gmail.com>
2022-07-11 18:57:41 +0300

    Remove unused variables

--- src/components/Department.js
+++ src/components/Department.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Department = ({ item }) => {
-  const location = useLocation();
   return (
     <div>
       <Link to={`/departments/${item.id}/groups`}>{item.name}</Link>
